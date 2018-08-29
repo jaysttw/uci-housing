b1bb084572e09a1d8a4f57893255f7f6b5a7547e
Jay <jayseetow@gmail.com>
2018-08-29 16:53:41 +0100

    Updated with complete linear regression model, ready for use in webapp.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,35 +1,60 @@
 import os
-from flask import Flask
+import numpy as np
+import sklearn
+from flask import Flask, request, jsonify, json, abort
 
-def create_app(test_config=None):
-    # Create and configure Flask app
-    app = Flask(__name__, instance_relative_config=True)
-    
-    @app.route('/predict', methods=['GET', 'POST'])
-    def predict():
-        request.get_data()
-        print(request.mimetype)
-        input = request.get_json(force=True)
-        if validate(input):
-            resp.status_code = 200
-        else:
-            return abort(400)
-        output = {'house_value': 1.0, 'stddev': 1.0}
-        resp = jsonify(output)
-        return resp
+app = Flask(__name__)
 
-    def validate(input):
-        labels = {'crime_rate',
-                  'avg_number_of_rooms',
-                  'distance_to_employment_center',
-                  'property_tax_rate',
-                  'pupil_teacher_ratio'
-                  }
-        if labels not in input:
-            return False
-        elif not all([isinstance(input[x], (int, float)) for x in labels]):
-            return False
-        else:
-            return True
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    request.get_data()
+    print(request.mimetype)
+    input = request.get_json(force=True)
+    if validate(input):
+        pass
+    else:
+        return abort(400)
+    output = model_prediction(input) #{'house_value': 1.0, 'stddev': 1.0}
+    resp = jsonify(output)
+    return resp
 
-    return app
+def validate(input):
+    labels = {'crime_rate',
+              'avg_number_of_rooms',
+              'distance_to_employment_center',
+              'property_tax_rate',
+              'pupil_teacher_ratio'
+              }
+    #print(f"Label check: {[x in input for x in labels]}")
+    #print(f"Type check: {[isinstance(input[x], (int, float)) for x in labels]}")
+    if not all([x in input for x in labels]):
+        return False
+    elif not all([isinstance(input[x], (int, float)) for x in labels]):
+        return False
+    else:
+        return True
+
+def model_prediction(input):
+    labels = ['crime_rate',
+              'avg_number_of_rooms',
+              'distance_to_employment_center',
+              'property_tax_rate',
+              'pupil_teacher_ratio'
+              ]
+    train = np.asarray([input[x] for x in labels]).reshape(1,-1)
+    model = sklearn.externals.joblib.load(
+        os.path.join(
+            os.path.pardir,
+            'model.pkl'
+        )
+    )
+    SD = sklearn.externals.joblib.load(
+        os.path.join(
+            os.path.pardir,
+            'SD.pkl'
+        )
+    )
+    prediction = model.predict(train)
+    result = {'house_value': prediction[0],
+              'stddev': SD}
+    return result
